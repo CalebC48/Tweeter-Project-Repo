2659c46282ce353b31d1f4dee258eb859ff4ab12
Caleb Calderwood <Calderwoodc@gmail.com>
2024-10-08 08:38:25 -0600

    Created StatusItemPresenters & Service

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -12,11 +12,13 @@ import Register from "./components/authentication/register/Register";
 import MainLayout from "./components/mainLayout/MainLayout";
 import Toaster from "./components/toaster/Toaster";
 import StatusItemScroller from "./components/mainLayout/StatusItemScroller";
-import { AuthToken, User, FakeData, Status } from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
 import { FolloweePresenter } from "./presenters/FolloweePresenter";
 import { UserItemView } from "./presenters/UserItemPresenter";
 import { FollowerPresenter } from "./presenters/FollowerPresenter";
+import { FeedPresenter } from "./presenters/FeedPresenter";
+import { StatusItemView } from "./presenters/StatusItemPresenter";
+import { StoryPresenter } from "./presenters/StoryPresenter";
 
 const App = () => {
   const { currentUser, authToken } = useUserInfo();
@@ -40,27 +42,6 @@ const App = () => {
 };
 
 const AuthenticatedRoutes = () => {
-  //put in status service
-  const loadMoreFeedItems = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: Status | null
-  ): Promise<[Status[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-  };
-
-  const loadMoreStoryItems = async (
-    authToken: AuthToken,
-    userAlias: string,
-    pageSize: number,
-    lastItem: Status | null
-  ): Promise<[Status[], boolean]> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-  };
-
   return (
     <Routes>
       <Route element={<MainLayout />}>
@@ -70,8 +51,9 @@ const AuthenticatedRoutes = () => {
           element={
             <StatusItemScroller
               key={1}
-              loadItems={loadMoreFeedItems}
-              itemDescription="feed"
+              presenterGenerator={(view: StatusItemView) =>
+                new FeedPresenter(view)
+              }
             />
           }
         />
@@ -80,8 +62,9 @@ const AuthenticatedRoutes = () => {
           element={
             <StatusItemScroller
               key={1}
-              loadItems={loadMoreStoryItems}
-              itemDescription="story"
+              presenterGenerator={(view: StatusItemView) =>
+                new StoryPresenter(view)
+              }
             />
           }
         />
--- tweeter-web/src/model/service/StatusService.ts
+++ tweeter-web/src/model/service/StatusService.ts
@@ -1,20 +1,23 @@
+import { AuthToken, Status, FakeData } from "tweeter-shared";
+
 export class StatusService {
-  //   loadMoreFeedItems = async (
-  //     authToken: AuthToken,
-  //     userAlias: string,
-  //     pageSize: number,
-  //     lastItem: Status | null
-  //   ): Promise<[Status[], boolean]> => {
-  //     // TODO: Replace with the result of calling server
-  //     return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-  //   };
-  //   loadMoreStoryItems = async (
-  //     authToken: AuthToken,
-  //     userAlias: string,
-  //     pageSize: number,
-  //     lastItem: Status | null
-  //   ): Promise<[Status[], boolean]> => {
-  //     // TODO: Replace with the result of calling server
-  //     return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-  //   };
+  public async loadMoreFeedItems(
+    authToken: AuthToken,
+    userAlias: string,
+    pageSize: number,
+    lastItem: Status | null
+  ): Promise<[Status[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+  }
+
+  public async loadMoreStoryItems(
+    authToken: AuthToken,
+    userAlias: string,
+    pageSize: number,
+    lastItem: Status | null
+  ): Promise<[Status[], boolean]> {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
+  }
 }
